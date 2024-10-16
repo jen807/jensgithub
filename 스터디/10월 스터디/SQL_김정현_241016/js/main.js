872b6abae3d5d6ed1125851db2f36e422c039f5c
jen807 <80chill@naver.com>
2024-10-16 12:01:12 +0900

    업데이트

---
+++ 스터디/10월 스터디/SQL_김정현_241016/js/main.js
@@ -0,0 +1,34 @@
+const likeEl = document.querySelector('.fa-regular.fa-heart');
+
+const liketoggle = () => {
+    likeEl.classList.toggle('fa-regular');
+    likeEl.classList.toggle('fa-solid');
+};
+
+likeEl.addEventListener('click', liketoggle);
+
+const formEl = document.querySelector('form');
+const commentInput = document.querySelector('.comment');
+const commentBox = document.querySelector('.comment_box');
+const saveComment = localStorage.length;
+
+formEl.addEventListener('submit', (e) => {
+            
+   e.preventDefault();
+            
+   const inputComment = commentInput.value;
+   const saveComment = localStorage.length;
+
+   if(inputComment !== null){
+        const newComment = document.createElement('p');
+        newComment.innerHTML = inputComment;
+        commentBox.append(newComment);
+        commentInput.value = "";
+
+    console.log('이거안되면바지에똥싼다');
+
+    localStorage.setItem(`COMMENT_${saveComment}`, inputComment);
+
+    }
+
+}); 
